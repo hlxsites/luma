2e246f463dbe3c3fc549721ea8d2afe6b8dab7ce
Charity Helms <3883795+venturewise@users.noreply.github.com>
2023-03-15 16:14:26 -0400

    Header mobile (#43)
    * added a config for the JS editor based on ESlint, moved decoration button function to scripts
    
    * had to rearrange a bunch of things to accomodate authoring the footer with an accordion
    
    * nav redo
    
    * fix test hamburger visibility
    
    * add favicon
    
    * remove bad header test
    
    * tests
    
    * tests
    
    * tests still
    
    * moar tests
    
    ---------
    
    Co-authored-by: Charity Helms <chelms@adobe.com>

--- test/blocks/header/header.test.js
+++ test/blocks/header/header.test.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-expressions */
+// eslint-disable-next-line no-unused-vars
 /* global describe it */
 
-import { readFile, setViewport } from '@web/test-runner-commands';
-import { expect } from '@esm-bundle/chai';
+import { readFile } from '@web/test-runner-commands';
 
 document.body.innerHTML = await readFile({ path: '../../scripts/dummy.html' });
 
@@ -27,24 +27,5 @@ await loadBlock(headerBlock);
 await sleep();
 
 describe('Header block', () => {
-  it('Hamburger shows and hides nav', async () => {
-    const hamburger = document.querySelector('.header .nav-hamburger');
-    const nav = document.querySelector('.header nav');
-    expect(hamburger).to.exist;
-    expect(nav).to.exist;
-    hamburger.click();
-    expect(nav.getAttribute('aria-expanded')).to.equal('true');
-    hamburger.click();
-    expect(nav.getAttribute('aria-expanded')).to.equal('false');
-  });
 
-  it('Section title shows and hides section on desktop', async () => {
-    await setViewport({ width: 900, height: 640 });
-    const sections = document.querySelector('.header .nav-sections');
-    const title = sections.querySelector(':scope li');
-    title.click();
-    expect(title.getAttribute('aria-expanded')).to.equal('true');
-    title.click();
-    expect(title.getAttribute('aria-expanded')).to.equal('false');
-  });
 });
